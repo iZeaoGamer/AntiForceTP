f5de0a829bdd280dc9dd4b5d0c94fb80f970f601
Angel <bubblicious712@gmail.com>
2017-09-24 14:51:28 -0400

    Update Main.php

--- src/Angel/AntiTP/Main.php
+++ src/Angel/AntiTP/Main.php
@@ -6,8 +6,6 @@ use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\level\Position;
-
 
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
 
@@ -20,30 +18,26 @@ class Main extends PluginBase implements Listener{
   }
   
   public function antiTP(PlayerCommandPreprocessEvent $ev){
+    $p = $ev->getPlayer();
     $command = strtolower(explode(" ", $ev->getMessage())[0]);
-    if($ev->getPlayer()->isOp() or $ev->getPlayer()->hasPermission("pocketmine.command.teleport")){
+    if($p->isOp() or $p->hasPermission("pocketmine.command.teleport")){
       if($command == "./tp" || $command == "/tp"){
-        $ev->getPlayer()->setGamemode(4);
-        $this->tper[strtolower($ev->getPlayer()->getName())] = strtolower($ev->getPlayer()->getName());
+        $p->setGamemode(4);
+        $this->tper[strtolower($p->getName())] = strtolower($p->getName());
       }
       // cancels event if force tped and run blocked force tp command
-      if(isset($this->tper[strtolower($ev->getPlayer()->getName())])){
+      if(isset($this->tper[strtolower($p->getName())])){
         if($command == "/sethome" || $command == "./sethome"){
           $ev->setCancelled(true);
-          $ev->getPlayer()->sendMessage("This Command is Disabled Since You Force Tped! , Remove this by tping to spawn!");
+          $p->sendMessage("This Command is Disabled Since You Force Tped! , Remove this by tping to spawn!");
         }
       }
       
       if($command == "/spawn" || $command == "./spawn"){
-        unset($this->tper[strtolower($ev->getPlayer()->getName())]);
-        $ev->getPlayer()->sendMessage("Forced TPed removed!");
-        $ev->getPlayer()->setGamemode(0);
+        unset($this->tper[strtolower($p->getName())]);
+        $p->sendMessage("Forced TPed removed!");
+        $p->setGamemode(0);
       }
     }
   }
 }
-        
-          
-      
-      
-          
