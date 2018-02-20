958915813c6d3a62f19f9ec8ab4abbb6d1d8c76d
iZeaoGamer <jameshayward14@icloud.com>
2018-02-20 17:00:14 +0000

    Fixed syntax error

--- src/Angel/AntiTP/Main.php
+++ src/Angel/AntiTP/Main.php
@@ -1,26 +1,22 @@
 <?php
-
 namespace Angel\AntiTP;
-
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
-
 class Main extends PluginBase implements Listener{
   
   public $tper = [];
   public $essentialspe;
   
   public function onEnable(){
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-  }
-  $this->essentialspe = $this->getServer()->getPluginManager()->getPlugin("EssentialsPE");
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      $this->essentialspe = $this->getServer()->getPluginManager()->getPlugin("EssentialsPE");
         if (!$this->essentialspe) {
             $this->getLogger()->info("Add EssentialsPE to disable the /god feature upon teleport.");
         }
+  }
   public function antiTP(PlayerCommandPreprocessEvent $ev){
     $p = $ev->getPlayer();
     $command = strtolower(explode(" ", $ev->getMessage())[0]);
@@ -46,4 +42,5 @@ class Main extends PluginBase implements Listener{
       }
     }
   }
+ }
 }
