d4e32d15c0b2cce30d7acb528dc00648d9b5e80c
iZeaoGamer <jameshayward14@icloud.com>
2019-02-14 10:00:33 +0000

    Update Main.php

--- src/Angel/AntiTP/Main.php
+++ src/Angel/AntiTP/Main.php
@@ -17,13 +17,12 @@ class Main extends PluginBase implements Listener{
     $command = strtolower(explode(" ", $ev->getMessage())[0]);
     if($p->isOp() or $p->hasPermission("pocketmine.command.teleport")){
       if($command == "./tp" || $command == "/tp"){
-        $p->setGamemode(0);
         $p->sendMessage("§aYou have teleported to a player! \n§aYou best not abuse your tp. Only use it to catch hackers.");
         $this->tper[strtolower($p->getName())] = strtolower($p->getName());
       }
       // cancels event if force tped and run blocked force tp command
       if(isset($this->tper[strtolower($p->getName())])){
-        if($command == "/home" || $command == "./home"){
+        if($command == "/home" || $command == "./home" || $command == "/getpos" || $command == "./getpos"){
           $ev->setCancelled(true);
           $p->sendMessage("§cThis command is blocked by the server because you force tped. We do not want abusers. To fix this, use /spawn. Please do not use this command again.\n§cYou could potentially get demoted. Who knows!\n§cIt's called tp raiding / tp abuse.");
         }
